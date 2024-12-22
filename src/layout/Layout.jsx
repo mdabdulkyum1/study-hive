e9a214231fc8875af01dd82963ea3b6ceaefa67e
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-12-22 09:03:43 +0600

    add dark and light class

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,10 +5,12 @@ import Footer from "../components/Footer/Footer"
 
 function Layout() {
   return (
+    <div className="light:bg-light-bg dark:bg-dark-bg">
     <div className="container mx-auto">
         <Navbar></Navbar>
         <Outlet></Outlet>
         <Footer></Footer>
+    </div>
     </div>
   )
 }
